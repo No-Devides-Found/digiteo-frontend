42e6a70aa62745249a0e40deb46ab6835e1daf91
haram8009 <haram8009@gmail.com>
2023-08-01 00:20:29 +0900

    temp: axios 예시

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 
 function Home() {
+  useEffect(() => {
+    axios
+      .post("account/signup/", {
+        username: "user",
+        password: "user",
+        email: "user@user.user",
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log("err", err);
+      });
+  }, []);
   return <div>홈페이지</div>;
 }
 
