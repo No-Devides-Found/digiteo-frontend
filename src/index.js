64c7622ac192548645cbcd765a6e1509bf6ae50a
haram8009 <haram8009@gmail.com>
2023-08-06 19:02:46 +0900

    feat: 테마 적용

--- src/index.js
+++ src/index.js
@@ -4,14 +4,19 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+
 import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./styles/theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <CssBaseline />
-      <App />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
---
+++ src/styles/theme.js
@@ -0,0 +1,29 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        "*": {
+          boxSizing: "border-box",
+          margin: 0,
+          padding: 0,
+        },
+        html: {
+          height: "100%",
+          width: "100%",
+        },
+        body: {
+          height: "100%",
+          width: "100%",
+        },
+        "#root": {
+          height: "100%",
+          width: "100%",
+        },
+      },
+    },
+  },
+});
+
+export default theme;
