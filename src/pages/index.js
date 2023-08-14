1173f6dae0e411b80130264d78003951b434d868
haram8009 <haram8009@gmail.com>
2023-08-14 18:29:15 +0900

    fix: 임포트 에러 수정

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
 export { default as Home } from "./Home/Home";
 export { default as Signin } from "./Signin/Signin";
 export { default as Signup } from "./Signup/Signup";
-export { default as Mypage } from "./Mypage/Mypage";
+export { default as MyPage } from "./MyPage/MyPage";
