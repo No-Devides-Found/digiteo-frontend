b63195ef64fb624f798aa10e4dfc3b2784833458
minsun24 <jms49526@gmail.com>
2023-08-14 17:20:10 +0900

    수정

--- src/components/index.js
+++ src/components/index.js
@@ -1,14 +1,12 @@
 export { default as EventCarousel } from "./Home/EventCarousel/EventCarousel";
-
-
-
 export { default as PlayGroundCarousel } from "./Home/PlayGroundCarousel/PlayGroundCarousel";
 export { default as Program } from "./Home/Program/Program";
 export { default as RankingBanner } from "./Home/RankingBanner/RankingBanner";
-export { default as Footer } from "./Footer/Footer";
 export { default as RelatedSiteCarousel } from "./RelatedSiteCarousel/RelatedSiteCarousel";
-export { default as Header } from "./Header/Header";
-export { default as NavBar } from "./NavBar/NavBar";
+
+export { default as Header } from "./common/Header/Header";
+export { default as NavBar } from "./common/NavBar/NavBar";
+export { default as Footer } from "./common/Footer/Footer";
 
 export { default as Paginations } from "./common/Paginations/Paginations";
 export { default as Comments } from "./common/Comments/Comments";
@@ -17,6 +15,7 @@ export { default as SearchBar } from "./common/SearchBar/SearchBar";
 export { default as Tags } from "./common/Tags/Tags"; 
 export { default as Chips } from "./common/Chips/Chips";
 export { default as Dropdown } from "./common/Dropdown/Dropdown";
+
 
 //Practice
 export { default as CreationTemplate } from "./Practice/CreationTemplate";
