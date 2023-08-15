43c2a0bb04c3757a39d663848828cf9a1f09923d
minsun24 <jms49526@gmail.com>
2023-08-15 15:57:45 +0900

    창작마루 메인홈 수정

--- src/pages/Practice/PracticeHome.jsx
+++ src/pages/Practice/PracticeHome.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {  useNavigate } from 'react-router-dom';
-import {Box, Grid, Typography, Button, Stack} from "@mui/material";
+import {Box, Grid, Typography, Button, Stack, Container,} from "@mui/material";
 import { styled } from "@mui/material/styles";
 
 
@@ -19,9 +19,10 @@ const RightGrid = styled(Grid)({
   height: "20rem",
 })
 const StyledButton = styled(Button)({
-  height: "5rem",
-  width: "8rem",
-  margin: "0.5rem",
+
+})
+const SubGrid = styled(Grid)({
+
 })
 const TitleTypography = styled(Typography)({
   backgroundColor:'#FF8A65',
@@ -31,7 +32,7 @@ const TitleTypography = styled(Typography)({
 })
 
 
-const PracticeHome = () => {
+function PracticeHome () {
   const navigate = useNavigate();
 
   const onClick=(e)=>{
@@ -40,49 +41,83 @@ const PracticeHome = () => {
 
   //해당 창작물 페이지로 이동
   return(
-      <MainBox>
-      <Grid container>
-        <Grid item xs> 
-        <TitleTypography variant="h3" mt={2}>창작마루</TitleTypography>
-        <Typography>창작마루 설명입니다.</Typography>
+    <Box>
+      <Grid container spacing={1}>
+        <Grid item xs>
+          <Grid container spacing={1}>
+            <Grid item xs={12}>
+              제목
+            </Grid>
+            <Grid item xs={12}>
+              내용
+            </Grid>
+          </Grid>
         </Grid>
-        <RightGrid item xs>
-          <Grid container>
-            <Grid item xs>
-              <Stack direction="row" spacing={2}>
-                <StyledButton value="video" onClick={onClick} variant="contained">
-                  영상창작물
-                </StyledButton>
-              </Stack>
-            </Grid>
-            <Grid item xs>
-              <Stack direction="row" spacing={2}>
-                <StyledButton value="img" onClick={onClick} variant="contained">
-                  이미지창작물
-                </StyledButton>
-              </Stack>
-            </Grid>
-          </Grid>
-          <Grid container>
-            <Grid item xs>
-              <Stack direction="row" spacing={2}>
-                <StyledButton value="voice"onClick={onClick}  variant="contained" >
-                  음성창작물
-                </StyledButton>
-              </Stack>
-            </Grid>
-            <Grid item xs>
-              <Stack direction="row" spacing={2}>
-                <StyledButton value="text" onClick={onClick} variant="contained" >
-                  문서창작물
-                </StyledButton>
-              </Stack>
-            </Grid>
-          </Grid>
-        </RightGrid>
+        <Grid item xs>
+          창작마루 버튼 4개 들어가는 부분
+        </Grid>
       </Grid>
-    </MainBox>
-  ); 
-}
 
-export default PracticeHome;
+      
+      <Grid container style={{backgroundColor:"#FFF5E5", padding:"1rem 10rem"}}>
+        <Grid item xs >
+          <Stack style={{backgroundColor: '#FF8A65', minWidth:'45%', padding:"1rem"}}>
+            <Typography variant="h4" fontFamily={"Krona One"} >CHANG</Typography>
+            <Typography variant="h4" fontFamily={"Krona One"} >JAC</Typography>
+            <Typography variant="h4" fontFamily={"Krona One"} >MA</Typography>
+            <Typography variant="h4" fontFamily={"Krona One"} >ROO</Typography>
+          </Stack>
+          
+          <Typography variant="subtitle1"> 
+            창작마루(실습)란?<br/>
+            디지털 놀이터(학습)에 1개 이상의 프로그램을 듣고<br/>
+            <strong>본인이 원하는 형태의 디지털 콘텐츠를 제작하여 </strong><br/>
+            공유할 수 있는 커뮤니티입니다. <br/>
+            놀이터 프로그램들은 마지막에 과제 제출 시 가이드라인과 <br/>
+            예시가 주어지지만 창작마루에는 본인이 학습한<br/>
+            프로그램들을 참고해 창작물을 생성하고 공유한다는 <br/>
+            점에서 차이가 있습니다. <br/>
+            이러한 활동을 통해 디지털 리터러시 역량 증진을 위한 <br/>
+            자기주도적 학습을 유도.
+          </Typography>
+        </Grid>
+
+        <Grid item xs>
+            <StyledButton 
+            value="video" 
+            onClick={onClick} 
+            variant="contained">
+                영상창작물
+            </StyledButton>
+
+
+            <StyledButton 
+            value="img" 
+            onClick={onClick} 
+            variant="contained">
+                이미지창작물
+            </StyledButton>
+
+            <StyledButton 
+            value="voice"
+            onClick={onClick}  
+            variant="contained" >
+                음성창작물
+            </StyledButton>
+            
+
+            <StyledButton 
+                value="text" 
+                onClick={onClick} 
+                variant="contained" >
+                문서창작물
+            </StyledButton>
+      </Grid>    
+    </Grid> 
+
+    
+  </Box>
+  );
+};
+
+export default PracticeHome;  
