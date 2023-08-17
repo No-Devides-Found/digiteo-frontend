ac9b4bbe2d82bf2f3321f2a5d1ec486001a51763
haram8009 <haram8009@gmail.com>
2023-08-17 23:01:40 +0900

    feat: 유저 내가쓴 글 api

--- src/recoil/user/index.js
+++ src/recoil/user/index.js
@@ -1,3 +1,4 @@
 export { default as userState } from "./atom";
 export { default as withProfile } from "./withProfile";
 export { default as withProgram } from "./withProgram";
+export { default as withPost } from "./withPost";
--- src/recoil/user/withPost.js
+++ src/recoil/user/withPost.js
@@ -3,16 +3,16 @@ import Api from "../../api/api";
 import curUser from "./atom";
 import { server } from "../../constants";
 
-const withProgram = selector({
-  key: "get/user-program",
+const withPost = selector({
+  key: "get/user-post",
   get: async ({ get }) => {
     const { pk, email } = get(curUser);
     if (!pk || !email) return;
-    const url = `${server}/programs/myprogram/${pk}/`;
+    const url = `${server}/posts/mypost/${pk}/`;
 
     const response = await Api.get(url);
     return response.data;
   },
 });
 
-export default withProgram;
+export default withPost;
