285a27879a29d395be3117eb78562aea54ea6639
haram8009 <haram8009@gmail.com>
2023-08-18 21:20:25 +0900

    temp: 임시

--- src/pages/Practice/PracticeHome.jsx
+++ src/pages/Practice/PracticeHome.jsx
@@ -1,138 +1,173 @@
-import React from 'react';
-import {  useNavigate } from 'react-router-dom';
-import {Box, Grid, Typography, Button,ButtonBase} from "@mui/material";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Box, Grid, Typography, Button, ButtonBase } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
-
 const MainGrid = styled(Grid)({
-  width:"100%",
-  margin:"3rem auto 3rem auto",
+  width: "100%",
+  margin: "3rem auto 3rem auto",
   backgroundColor: "#FFF5E5",
-  padding:"4rem 6rem",
+  padding: "4rem 6rem",
 });
 
 const StyledContent = styled(Typography)({
-  fontSize:"0.8rem",
-  lineHeight:"2rem",
-  fontFamily:"Kumbh Sans"
-
+  fontSize: "0.8rem",
+  lineHeight: "2rem",
+  fontFamily: "Kumbh Sans",
 });
-const BrownGrid=styled(Grid)({
-  backgroundColor:"#A1887F", 
-  borderRadius:"20px",
-  position:"relative", 
-  width:"100%", 
-  height:"100%", 
-  margin:"0 auto"
-})
-const GreenGrid=styled(Grid)({
-  backgroundColor:"#DCEDC8", 
-  borderRadius:"20px", 
-  justifyContent:"center",
-  position:"relative",
-   margin:"1.5rem", 
-   alignItems:'center', 
-   padding:"1rem 1rem 1rem 0"
-})
+const BrownGrid = styled(Grid)({
+  backgroundColor: "#A1887F",
+  borderRadius: "20px",
+  position: "relative",
+  width: "100%",
+  height: "100%",
+  margin: "0 auto",
+});
+const GreenGrid = styled(Grid)({
+  backgroundColor: "#DCEDC8",
+  borderRadius: "20px",
+  justifyContent: "center",
+  position: "relative",
+  margin: "1.5rem",
+  alignItems: "center",
+  padding: "1rem 1rem 1rem 0",
+});
 
-
-
-
-function PracticeHome () {
+function PracticeHome() {
   const navigate = useNavigate();
 
-  //프로그램 유형 버튼 클릭 
+  // 프로그램 유형 버튼 클릭
   //-> 유형(value) 전달하면서 창작마루 목록으로 이동
-  const onClick=(e)=>{
-    console.log(e.target.alt)
-    navigate(`/practicelist?param1=${e.target.alt}`);   
-  }
+  const onClick = (e) => {
+    console.log(e.target.alt);
+    navigate(`/practicelist?param1=${e.target.alt}`);
+  };
 
   //해당 창작물 페이지로 이동
-  return(
-    <Box style={{height:"100vh"}}>
-      <MainGrid container >
-        <Grid item xs={5} >
+  return (
+    <Box style={{ height: "100vh" }}>
+      <MainGrid container>
+        <Grid item xs={5}>
           {/* 창작마루 좌측 타이틀 - 내용 */}
-          <Grid container style={{paddingLeft:"3rem"}}>
+          <Grid container style={{ paddingLeft: "3rem" }}>
             <Grid item xs={12}>
-              <Typography variant="h4" fontFamily={"Krona One"} 
-                style={{backgroundColor:"#FF8A65", width:"40%", padding:"1rem"
-              }}>
-                CHANG <br/>
-                JAC <br/>
-                MA <br/>
-                ROO <br/> 
+              <Typography
+                variant="h4"
+                fontFamily={"Krona One"}
+                style={{
+                  backgroundColor: "#FF8A65",
+                  width: "40%",
+                  padding: "1rem",
+                }}
+              >
+                CHANG <br />
+                JAC <br />
+                MA <br />
+                ROO <br />
               </Typography>
             </Grid>
-                        
-            <Grid item xs={12} style={{fontFamily:"Kumbh Sans", fontSize:"8"}} mt={2}>
-            <StyledContent variant="subtitle1"> 
-              <strong>창작마루</strong>(실습)란?<br/>
-              <strong>디지털 놀이터</strong>(학습)에 1개 이상의 프로그램을 듣고<br/>
-              <strong>본인이 원하는 형태의 디지털 콘텐츠를 제작하여 <br/>
-              공유할 수 있는 커뮤니티</strong>입니다. <br/>
-              놀이터 프로그램들은 마지막에 과제 제출 시 가이드라인과 <br/>
-              예시가 주어지지만 창작마루에는 본인이 학습한<br/>
-              프로그램들을 참고해 창작물을 생성하고 공유한다는 <br/>
-              점에서 차이가 있습니다. <br/>
-              이러한 활동을 통해 디지털 리터러시 역량 증진을 위한 <br/>
-              자기주도적 학습을 유도.
-            </StyledContent>
-            
-                  
+
+            <Grid
+              item
+              xs={12}
+              style={{ fontFamily: "Kumbh Sans", fontSize: "8" }}
+              mt={2}
+            >
+              <StyledContent variant="subtitle1">
+                <strong>창작마루</strong>(실습)란?
+                <br />
+                <strong>디지털 놀이터</strong>(학습)에 1개 이상의 프로그램을
+                듣고
+                <br />
+                <strong>
+                  본인이 원하는 형태의 디지털 콘텐츠를 제작하여 <br />
+                  공유할 수 있는 커뮤니티
+                </strong>
+                입니다. <br />
+                놀이터 프로그램들은 마지막에 과제 제출 시 가이드라인과 <br />
+                예시가 주어지지만 창작마루에는 본인이 학습한
+                <br />
+                프로그램들을 참고해 창작물을 생성하고 공유한다는 <br />
+                점에서 차이가 있습니다. <br />
+                이러한 활동을 통해 디지털 리터러시 역량 증진을 위한 <br />
+                자기주도적 학습을 유도.
+              </StyledContent>
             </Grid>
           </Grid>
         </Grid>
 
         {/* 창작마루 우측 - 유형 버튼 */}
-        <Grid item xs={7} style={{posigion:"relative"}}>
-        
-          <BrownGrid container style={{justifyContent:"center"}}>
-          <img src="img/changjactitle.png" alt="changjac-title" 
-              style={{position:"absolute", width:"35%", top:"-6%", zIndex:"10",
-              }}/>
-            <GreenGrid container >
-              
-              <Grid item xs={6} >
-                <img src="img/video.png" 
-                alt="video" 
-                style={{width:"80%", display:"block", margin:"0 auto",cursor:"pointer"}}
-                onClick={onClick} 
+        <Grid item xs={7} style={{ posigion: "relative" }}>
+          <BrownGrid container style={{ justifyContent: "center" }}>
+            <img
+              src="img/changjactitle.png"
+              alt="changjac-title"
+              style={{
+                position: "absolute",
+                width: "35%",
+                top: "-6%",
+                zIndex: "10",
+              }}
+            />
+            <GreenGrid container>
+              <Grid item xs={6}>
+                <img
+                  src="img/video.png"
+                  alt="video"
+                  style={{
+                    width: "80%",
+                    display: "block",
+                    margin: "0 auto",
+                    cursor: "pointer",
+                  }}
+                  onClick={onClick}
                 />
               </Grid>
               <Grid item xs={6}>
-                <img src="img/video.png" 
-                alt="img" 
-                style={{width:"80%", display:"block", margin:"0 auto",cursor:"pointer"}}
-                onClick={onClick}
+                <img
+                  src="img/video.png"
+                  alt="img"
+                  style={{
+                    width: "80%",
+                    display: "block",
+                    margin: "0 auto",
+                    cursor: "pointer",
+                  }}
+                  onClick={onClick}
                 />
               </Grid>
               <Grid item xs={6}>
-                <img src="img/video.png" 
-                alt="voice"
-                style={{width:"80%", display:"block", margin:"0 auto", cursor:"pointer"}}
+                <img
+                  src="img/video.png"
+                  alt="voice"
+                  style={{
+                    width: "80%",
+                    display: "block",
+                    margin: "0 auto",
+                    cursor: "pointer",
+                  }}
+                  onClick={onClick}
                 />
-        
               </Grid>
               <Grid item xs={6}>
-                <img src="img/video.png" 
-                alt="text"
-                style={{width:"80%", display:"block", margin:"0 auto", cursor:"pointer"}}
-                onClick={onClick} 
+                <img
+                  src="img/video.png"
+                  alt="text"
+                  style={{
+                    width: "80%",
+                    display: "block",
+                    margin: "0 auto",
+                    cursor: "pointer",
+                  }}
+                  onClick={onClick}
                 />
-
               </Grid>
             </GreenGrid>
-
           </BrownGrid>
         </Grid>
       </MainGrid>
-
-    
-    
-  </Box>
+    </Box>
   );
-};
+}
 
-export default PracticeHome;  
+export default PracticeHome;
--- src/pages/Practice/PracticeList.jsx
+++ src/pages/Practice/PracticeList.jsx
@@ -1,120 +1,118 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { styled } from "@mui/material/styles";
 
-import { useNavigate, useLocation} from 'react-router-dom';
-import {Box, Grid, Typography, Button, ButtonBase, Paper} from "@mui/material";
-import { ListTemplate, PracticeNavbar, SearchBar, Tags, Chips, Dropdown, Paginations} from '../../components';
-
+import { useNavigate, useLocation } from "react-router-dom";
+import {
+  Box,
+  Grid,
+  Typography,
+  Button,
+  ButtonBase,
+  Paper,
+} from "@mui/material";
+import {
+  ListTemplate,
+  PracticeNavbar,
+  SearchBar,
+  Tags,
+  Chips,
+  Dropdown,
+  Paginations,
+} from "../../components";
 
 const BackBox = styled(Box)({
   left: "0px",
   top: "297px",
-  background: "linear-gradient(180deg, #FFFDE7 0%, rgba(217, 217, 217, 0) 100%)",
-})
+  background:
+    "linear-gradient(180deg, #FFFDE7 0%, rgba(217, 217, 217, 0) 100%)",
+});
 
+const BrownGrid = styled(Grid)({
+  backgroundColor: "#A1887F",
+  borderRadius: "20px",
+  position: "relative",
+  width: "100%",
+  height: "100%",
+  margin: "0 auto",
+});
+const GreenGrid = styled(Grid)({
+  backgroundColor: "#DCEDC8",
+  borderRadius: "20px",
+  justifyContent: "center",
+  position: "relative",
+  margin: "1.5rem",
+  alignItems: "center",
+  padding: "1rem 1rem 1rem 0",
+});
 
-
-const BrownGrid=styled(Grid)({
-  backgroundColor:"#A1887F", 
-  borderRadius:"20px",
-  position:"relative", 
-  width:"100%", 
-  height:"100%", 
-  margin:"0 auto"
-})
-const GreenGrid=styled(Grid)({
-  backgroundColor:"#DCEDC8", 
-  borderRadius:"20px", 
-  justifyContent:"center",
-  position:"relative",
-   margin:"1.5rem", 
-   alignItems:'center', 
-   padding:"1rem 1rem 1rem 0"
-})
-
-
-
-const options = ["제목+닉네임", "제목", "닉네임"]
-
-
-
+const options = ["제목+닉네임", "제목", "닉네임"];
 
 function PracticeList() {
-
-  
-  
   const location = useLocation();
   const params = new URLSearchParams(location.search);
-  const firtType = params.get('param1');  // 창작마루 홈에서 진입할 때 받는 유형 정보
-  
-  //창작물 유형 정보 -> // page 유형에 따라 가져올 리스트 달라짐
-  const [page, setPage]=useState(firtType) 
-  
-  const [practiceList, setPracticeList] = useState([]); 
+  const firtType = params.get("param1"); // 창작마루 홈에서 진입할 때 받는 유형 정보
 
-  
+  //창작물 유형 정보 -> // page 유형에 따라 가져올 리스트 달라짐
+  const [page, setPage] = useState(firtType);
+
+  const [practiceList, setPracticeList] = useState([]);
+
   // 프로그램 목록 불러오기
   const [programs, setPrograms] = useState([
     {
-      id:"",
-      title:'프로그램명1',
-
+      id: "",
+      title: "프로그램명1",
     },
     {
-      id:"",
-      title:'프로그램명1',
+      id: "",
+      title: "프로그램명1",
     },
     {
-      id:"",
-      title:'프로그램명1',
-    }
+      id: "",
+      title: "프로그램명1",
+    },
   ]);
 
   //const [programs, setPrograms]=useState([]);
 
-
-
-  // 검색바 관련 상태값 
+  // 검색바 관련 상태값
   const [choice, setChoice] = useState(0); //선택 검색 옵션 : (0)제목+닉네임, (1)제목, (2)닉네임
-  const [searchKeyword, setSearchKeyword ]= useState(''); //검색어
-  const [isSearch, setIsSearch]=useState(false); //검색버튼 클릭 여부
-  
-  // 태그 검색 관련 상태값  
+  const [searchKeyword, setSearchKeyword] = useState(""); //검색어
+  const [isSearch, setIsSearch] = useState(false); //검색버튼 클릭 여부
+
+  // 태그 검색 관련 상태값
   const [searchTags, setSearchTags] = useState([]); //빈배열 초기화
-  
 
   // 리스트 정렬 드롭다운 관련 상태값
   const [order, setOrder] = useState(0); //초기화 : (0)최신순 / (1)좋아요순
 
+  const getPracticeList = async () => {
+    try {
+      // const resp = (await axios.get("/posts/practice-list")).data; ////localhost:3000/practicelist
+      const resp = await axios.get("/posts/practice/"); // 임시로 검색어상관없이 모든 창작게시물 불러오는 코드
 
+      console.log(resp.data);
+      setPracticeList(resp.data); //practiceList 변수에 할당
+    } catch (error) {
+      console.error("Error fetching practice list:", error);
+    }
+  };
 
+  useEffect(() => {
+    getPracticeList(); //창작물 목록 조회 함수 호출
+  }, [choice, searchTags, order]);
 
-
-  // const getPracticeList = async () => {
-  //   try{
-  //     const resp = (await axios.get('/practicelist')).data ////localhost:3000/practicelist
-  //   setPracticeList(resp.data);  //practiceList 변수에 할당
-    
-
-  //   }catch (error) {
-  //   console.error('Error fetching practice list:', error);
-  //   }
-  // }
-  
-  // useEffect(() => {
-  //   getPracticeList(); //창작물 목록 조회 함수 호출
-  // }, []); 
-
-
-
-  
   return (
     <BackBox>
       <Grid container style={{ padding: "5rem 13rem" }}>
         {/* 창작마루 타이틀 이미지 */}
-        <Grid item xs={12} style={{ justifyContent: 'center' }} >
-          <img src="img/changjactitle.png" alt="changjac-title" style={{ width: "35%", display: "block", margin: "0 auto" }} />
+        <Grid item xs={12} style={{ justifyContent: "center" }}>
+          <img
+            src="img/changjactitle.png"
+            alt="changjac-title"
+            style={{ width: "35%", display: "block", margin: "0 auto" }}
+          />
         </Grid>
 
         {/* 네브바 */}
@@ -124,39 +122,43 @@ function PracticeList() {
 
         {/* 검색바 */}
         <Grid item xs={12} mt={9}>
-          <SearchBar options={options}
-            choice={choice} setChoice={setChoice}
-            searchKeyword={searchKeyword} setSearchKeyword={setSearchKeyword}
-            isSearch={isSearch} setIsSearch={setIsSearch}
+          <SearchBar
+            options={options}
+            choice={choice}
+            setChoice={setChoice}
+            searchKeyword={searchKeyword}
+            setSearchKeyword={setSearchKeyword}
+            isSearch={isSearch}
+            setIsSearch={setIsSearch}
           />
         </Grid>
 
         {/* 프로그램 태그 검색 */}
-        <Grid item xs={12} mt={5} style={{ display: "flex", justifyContent: "space-between" }}>
+        <Grid
+          item
+          xs={12}
+          mt={5}
+          style={{ display: "flex", justifyContent: "space-between" }}
+        >
           <Chips programs={programs} />
           {/* <Tags programs={programs} searchTags={searchTags} setSearchTags={setSearchTags} /> */}
-          <Dropdown style={{ width: "100%" }} order={order} setOrder={setOrder} />
+          <Dropdown
+            style={{ width: "100%" }}
+            order={order}
+            setOrder={setOrder}
+          />
         </Grid>
-        
-
 
         {/* 창작물 리스트 들어가는 공간 */}
         <Grid item xs={12} mt={5}>
           <BrownGrid container style={{ justifyContent: "center" }}>
             <GreenGrid container>
-              <ListTemplate 
-              page={page} searchKeyword={searchKeyword} order={order} searchTags={searchTags} />
+              <ListTemplate practices={practiceList} page={page} />
             </GreenGrid>
           </BrownGrid>
         </Grid>
-        
       </Grid>
     </BackBox>
-    
   );
 }
 export default PracticeList;
-
-
-
-
